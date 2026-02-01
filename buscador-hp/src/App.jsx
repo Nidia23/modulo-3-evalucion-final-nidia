7a044dc6f2cf74146c287fd72af5ec43f4c7d603
Nidia23 <nidia_ibz_23hotmail.com>
2026-02-01 13:26:23 +0100

    inicios

---
+++ buscador-hp/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import hpLogo from '/hp.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <section>
+        <div>
+          <form>
+            <label for='name'>Encuentra tu personaje: </label>
+            <input type='text' placeholder='Harry Potter'></input>
+            <label for='name'>¿A qué casa pertenece?: </label>
+            <select name='filter-house' id='houses'>
+              <option value='Gryffindor'>Gryffindor</option>
+              <option value='Slytherin'>Slytherin</option>
+              <option value='Hufflepuff'>Hufflepuff</option>
+              <option value='Ravenclaw'>Ravenclaw</option>
+            </select>
+          </form>
+        <button>Buscar</button>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
